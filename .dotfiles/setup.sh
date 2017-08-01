61c311e10daab0cdb25c6fb87f0c4542ddcd9750
Tyson Harding <tharding@cs.utah.edu>
2017-08-01 14:19:41 -0600

    Add a setup file for dotfiles

---
+++ .dotfiles/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+git clone --bare https://github.com/tharding/dotfiles.git $HOME/.cfg
+
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+mkdir -p .config-backup
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
+
+# Add profiles to the user's environment
+echo "for f in ~/.profiles/*; do source \$f; done" >> ~/.bash_profile
